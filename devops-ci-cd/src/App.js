ccc5618ba5144612427697d8cc856a875f00c191
Ervin <70893070+Er4ik@users.noreply.github.com>
2022-12-14 00:42:25 +0200

    fix: run prettier

--- devops-ci-cd/src/App.js
+++ devops-ci-cd/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
   return (
-      <div className="App">
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>Hello, it's Er4ik here 😎</p>
-          </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello, it's Er4ik here 😎</p>
+      </header>
+    </div>
   );
 }
 
